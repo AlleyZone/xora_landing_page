7016ec411ece1f2db958e4e879b65232057a81f6
Alley <llzeynolla@vk.com>
2024-12-19 20:13:39 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './sections/Header.jsx'
+import Hero from './sections/Hero.jsx'
+import Features from './sections/Features.jsx'
+
+const App = () => {
+    return (
+        <div className="">
+            <Header />
+            <Hero />
+            <Features />
+        </div>
+    )
+}
+export default App
